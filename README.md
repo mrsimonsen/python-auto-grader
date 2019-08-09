ed55edf1a2b941c0dd0ef1cd6f3ad9c0a138d708
Mr. Simonsen <johnsimonsen@weber.edu>
2019-08-09 17:43:07 -0600

    got started

---
+++ README.md
@@ -0,0 +1,17 @@
+support tool for my python programming class.
+
+outline of the program:
+
+* Display reminder to run (this) program from the parent directory of all student repos
+* ask for the path of the student assignment (all students should be using the same directory structure and file name)
+* ask for the name of the test file (not the one given to students to ensure testing integrity)
+* run the grading
+* display if there were any folders missing the assignment
+* save a file with the results of the tests per student
+
+
+Additional features:
+* ask if you want a file copy of folders missing assignment
+* create a files of all assignment paths and make a menu to choose assignment
+* create a file that is a collection of student object that link grades to assignment, student names to usernames, and class period
+* add testing when typing in files to make sure they exist before running grading
---
+++ grader.py
@@ -0,0 +1,38 @@
+def intro():
+    print("Python Grader")
+    print("This program needs to be ran from the parent directory of the collection of student repos")
+    print()
+    assignment = input("What is the path of the assignment?\n")
+    test = input("What is the name of the test file?\n")
+    return test, assignment
+
+def outro(errors):
+    print("Testing complete")
+    if errors:
+        print(f"There were {len(errors)} errors:")
+        for i in errors:
+            print(i)
+
+def grade(test, path):
+    errors = []
+    #for each folder in the directory
+        #change working directory to folder/assignment
+        #set collection of output
+        #run tests
+        #save output to file under folder name
+        #if can't run tests
+            #add folder name to errors
+    #close file
+    #return name list
+    return errors
+
+
+
+
+def main():
+    test, assignment = intro()
+    errors = grade(test, assignment)
+    outro(errors)
+
+if __name__ == '__main__':
+    main()
